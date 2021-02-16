df44f9dbca9c6a2377f69bcc067e4a3a9ee84a2e
Shengda <dingshengda@gmail.com>
2021-02-15 21:55:29 -0800

    Test redeem with fee

--- test/BTC+.test.js
+++ test/BTC+.test.js
@@ -1,4 +1,4 @@
-const { expectRevert } = require('@openzeppelin/test-helpers');
+const { expectRevert, BN } = require('@openzeppelin/test-helpers');
 const assert = require('assert');
 const Pool = artifacts.require("SinglePool");
 const BTCPlus = artifacts.require("BTCPlus");
@@ -6,6 +6,13 @@ const ERC20 = artifacts.require("MockToken");
 const Strategy = artifacts.require("MockStrategy");
 
 const MAX = web3.utils.toBN(2).pow(web3.utils.toBN(256)).sub(web3.utils.toBN(1));
+const assertAlmostEqual = function(expectedOrig, actualOrig) {
+    const _1e18 = new BN('10').pow(new BN('18'));
+    const expected = new BN(expectedOrig).div(_1e18).toNumber();
+    const actual = new BN(actualOrig).div(_1e18).toNumber();
+
+    assert.ok(Math.abs(expected - actual) <= 2, `Expected ${expected}, actual ${actual}`);
+}
 
 contract("BTC+", async ([owner, treasury, strategist, user1, user2, user3, btcPlus2]) => {
     let btcPlus;
@@ -294,5 +301,115 @@ contract("BTC+", async ([owner, treasury, strategist, user1, user2, user3, btcPl
         // User2 should get 2 token1 and 4 token2
         assert.strictEqual((await token1.balanceOf(user2)).toString(), "2000000");
         assert.strictEqual((await token2.balanceOf(user2)).toString(), web3.utils.toWei("4"));
+    });
+
+    it("should redeem BTC+ with fee", async () => {
+        // Mint 20 token1 to user1
+        await token1.mint(user1, "20000000");
+        await token1.approve(btcPlus.address, "20000000", {from: user1});
+        // Mint 30 token2 to user1
+        await token2.mint(user1, web3.utils.toWei("30"));
+        await token2.approve(btcPlus.address, web3.utils.toWei("30"), {from: user1});
+        // Mint 50 BTC+ to user1
+        await btcPlus.mint([token1.address, token2.address], ["20000000", web3.utils.toWei("30")], {from: user1});
+        // Deposit token1 and token2 into strategies
+        await pool1.invest();
+        await pool2.invest();
+        // Harvest 2 token1 in strategy1
+        await token1.mint(strategy1.address, "2000000");
+        // Harvest 8 token2 in strategy2
+        await token2.mint(strategy2.address, web3.utils.toWei("8"));
+        await btcPlus.rebase();
+
+        // Mint 6 token2 to user2
+        await token2.mint(user2, web3.utils.toWei("6"));
+        await token2.approve(btcPlus.address, web3.utils.toWei("6"), {from: user2});
+        // Mint 6 BTC+ to user2
+        await btcPlus.mint([token2.address, token1.address], [web3.utils.toWei("6"), 0], {from: user2});
+        assert.strictEqual((await btcPlus.index()).toString(), web3.utils.toWei("1.2"));
+        assert.strictEqual((await btcPlus.balanceOf(user2)).toString(), web3.utils.toWei("6"));
+        assert.strictEqual((await btcPlus.totalSupply()).toString(), web3.utils.toWei("66"));
+        assert.strictEqual((await btcPlus.userShare(user2)).toString(), web3.utils.toWei("5"));
+        assert.strictEqual((await btcPlus.totalShares()).toString(), web3.utils.toWei("55"));
+        assert.strictEqual((await token1.balanceOf(user2)).toString(), "0");
+        assert.strictEqual((await token2.balanceOf(user2)).toString(), "0");
+
+        // Set redeem fee to 1%
+        await btcPlus.setRedeemFee(100);
+
+        // Redeems 2.4 BTC+ from user2
+        await btcPlus.redeem(web3.utils.toWei("2.4"), {from: user2});
+        assert.strictEqual((await btcPlus.index()).toString(), web3.utils.toWei("1.2"));
+        assert.strictEqual((await btcPlus.balanceOf(user2)).toString(), web3.utils.toWei("3.6"));
+        assert.strictEqual((await btcPlus.totalSupply()).toString(), web3.utils.toWei("63.6"));
+        assert.strictEqual((await btcPlus.userShare(user2)).toString(), web3.utils.toWei("3"));
+        assert.strictEqual((await btcPlus.totalShares()).toString(), web3.utils.toWei("53"));
+
+        // User2 should get 0.792 token1 and 1.584 token2
+        assert.strictEqual((await token1.balanceOf(user2)).toString(), "792000");
+        assert.strictEqual((await token2.balanceOf(user2)).toString(), web3.utils.toWei("1.584"));
+
+        // Should collect the fees after another round of rebase!
+        await btcPlus.rebase();
+        // Collect 2.4 * 1% = 0.024 BTC+ from redeem fee.
+        // 63.6 + 0.024 = 63.624
+        assert.strictEqual((await btcPlus.totalShares()).toString(), web3.utils.toWei("53"));
+        assertAlmostEqual((await btcPlus.totalSupply()).toString(), web3.utils.toWei("63.624"));
+        assertAlmostEqual((await btcPlus.index()).toString(), web3.utils.toWei("1.20045"));
+    });
+
+    it("should redeem all BTC+ with fee", async () => {
+        // Mint 20 token1 to user1
+        await token1.mint(user1, "20000000");
+        await token1.approve(btcPlus.address, "20000000", {from: user1});
+        // Mint 30 token2 to user1
+        await token2.mint(user1, web3.utils.toWei("30"));
+        await token2.approve(btcPlus.address, web3.utils.toWei("30"), {from: user1});
+        // Mint 50 BTC+ to user1
+        await btcPlus.mint([token1.address, token2.address], ["20000000", web3.utils.toWei("30")], {from: user1});
+        // Deposit token1 and token2 into strategies
+        await pool1.invest();
+        await pool2.invest();
+        // Harvest 2 token1 in strategy1
+        await token1.mint(strategy1.address, "2000000");
+        // Harvest 8 token2 in strategy2
+        await token2.mint(strategy2.address, web3.utils.toWei("8"));
+        await btcPlus.rebase();
+
+        // Mint 6 token2 to user2
+        await token2.mint(user2, web3.utils.toWei("6"));
+        await token2.approve(btcPlus.address, web3.utils.toWei("6"), {from: user2});
+        // Mint 6 BTC+ to user2
+        await btcPlus.mint([token2.address, token1.address], [web3.utils.toWei("6"), 0], {from: user2});
+        assert.strictEqual((await btcPlus.index()).toString(), web3.utils.toWei("1.2"));
+        assert.strictEqual((await btcPlus.balanceOf(user2)).toString(), web3.utils.toWei("6"));
+        assert.strictEqual((await btcPlus.totalSupply()).toString(), web3.utils.toWei("66"));
+        assert.strictEqual((await btcPlus.userShare(user2)).toString(), web3.utils.toWei("5"));
+        assert.strictEqual((await btcPlus.totalShares()).toString(), web3.utils.toWei("55"));
+        assert.strictEqual((await token1.balanceOf(user2)).toString(), "0");
+        assert.strictEqual((await token2.balanceOf(user2)).toString(), "0");
+
+        // Set redeem fee to 2.5%
+        await btcPlus.setRedeemFee(250);
+
+        // Redeems all BTC+ from user2
+        await btcPlus.redeem(MAX, {from: user2});
+        assert.strictEqual((await btcPlus.index()).toString(), web3.utils.toWei("1.2"));
+        assert.strictEqual((await btcPlus.balanceOf(user2)).toString(), web3.utils.toWei("0"));
+        assert.strictEqual((await btcPlus.totalSupply()).toString(), web3.utils.toWei("60"));
+        assert.strictEqual((await btcPlus.userShare(user2)).toString(), web3.utils.toWei("0"));
+        assert.strictEqual((await btcPlus.totalShares()).toString(), web3.utils.toWei("50"));
+
+        // User2 should get 1.95 token1 and 3.9 token2
+        assert.strictEqual((await token1.balanceOf(user2)).toString(), "1950000");
+        assert.strictEqual((await token2.balanceOf(user2)).toString(), web3.utils.toWei("3.9"));
+
+        // Should collect the fees after another round of rebase!
+        await btcPlus.rebase();
+        // Collect 6 * 2.5% = 0.15 BTC+ from redeem fee.
+        // 60 + 0.15 = 60.15
+        assert.strictEqual((await btcPlus.totalShares()).toString(), web3.utils.toWei("50"));
+        assert.strictEqual((await btcPlus.totalSupply()).toString(), web3.utils.toWei("60.15"));
+        assert.strictEqual((await btcPlus.index()).toString(), web3.utils.toWei("1.203"));
     });
 });
