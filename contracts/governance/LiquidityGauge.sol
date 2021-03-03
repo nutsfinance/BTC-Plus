4c790c334b9c7f4cf55d2340b13d7c921712da68
Shengda <dingshengda@gmail.com>
2021-03-03 13:39:05 -0800

    Deposit and withdraw

--- contracts/governance/LiquidityGauge.sol
+++ contracts/governance/LiquidityGauge.sol
@@ -11,6 +11,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol
 import "../interfaces/IGaugeController.sol";
 import "../interfaces/IGauge.sol";
 import "../interfaces/IUniPool.sol";
+import "../interfaces/IVotingEscrow.sol";
 
 /**
  * @dev Liquidity gauge that stakes token and earns reward.
@@ -24,6 +25,8 @@ abstract contract LiquidityGauge is ERC20Upgradeable, IGauge {
 
     event LiquidityLimitUpdated(address indexed account, uint256 balance, uint256 supply, uint256 oldWorkingSupply,
         uint256 oldWorkingBalance, uint256 newWorkingBalance, uint256 newWorkingSupply);
+    event Deposited(address indexed account, uint256 amount);
+    event Withdrawn(address indexed account, uint256 amount);
 
     uint256 constant TOKENLESS_PRODUCTION = 40;
     uint256 constant WAD = 10**18;
@@ -145,18 +148,25 @@ abstract contract LiquidityGauge is ERC20Upgradeable, IGauge {
     }
 
     /**
-     * @dev Performs global checkpoint for the liquidity gauge.
-     * Note: AC emission rate change is triggered by gauge controller. Each time there is a rate change,
-     * Gauge controller will checkpoint the gauge. Therefore, we could assume that the rate is not changed
-     * between two checkpoints!
+     * @dev Performs checkpoint on AC rewards.
      */
-    function checkpoint() public override {
+    function _checkpoint() internal {
         uint256 diffTime = block.timestamp.sub(lastCheckpoint);
         if (diffTime > 0) {
             uint256 diff = rate.mul(diffTime).div(workingSupply);
             integral = integral.add(diff);
             lastCheckpoint = block.timestamp;
         }
+    }
+
+    /**
+     * @dev Performs global checkpoint for the liquidity gauge.
+     * Note: AC emission rate change is triggered by gauge controller. Each time there is a rate change,
+     * Gauge controller will checkpoint the gauge. Therefore, we could assume that the rate is not changed
+     * between two checkpoints!
+     */
+    function checkpoint() public override {
+        _checkpoint();
 
         // Loads the new emission rate from gauge controller
         rate = IGaugeController(controller).gaugeRates(address(this));
@@ -203,4 +213,66 @@ abstract contract LiquidityGauge is ERC20Upgradeable, IGauge {
     function claimRewards(address _account) public {
         _checkpointRewards(_account);
     }
+
+    /**
+     * @dev Kicks an account for abusing their boost. Only kick if the user
+     * has another voting event, or their lock expires.
+     */
+    function kick(address _account) public {
+        IVotingEscrow escrow = IVotingEscrow(votingEscrow);
+        uint256 lastUserCheckpoint = checkpointOf[_account];
+        uint256 lastUserEvent = escrow.user_point_history__ts(_account, escrow.user_point_epoch(_account));
+
+        require(IERC20Upgradeable(address(escrow)).balanceOf(_account) == 0 || lastUserEvent > lastUserCheckpoint, "kick not allowed");
+
+        _checkpoint();
+        _checkpointUser(_account);
+        _updateLiquidityLimit(_account);
+    }
+
+    /**
+     * @dev Deposit the staked token into liquidity gauge.
+     * @param _amount Amount of token to deposit.
+     */
+    function deposit(uint256 _amount) public {
+        require(_amount > 0, "zero amount");
+
+        _checkpoint();
+        _checkpointUser(msg.sender);
+        _checkpointRewards(msg.sender);
+
+        _mint(msg.sender, _amount);
+        _updateLiquidityLimit(msg.sender);
+
+        IERC20Upgradeable(token).safeTransferFrom(msg.sender, address(this), _amount);
+
+        if (rewardContract != address(0x0)) {
+            IUniPool(rewardContract).stake(_amount);
+        }
+
+        emit Deposited(msg.sender, _amount);
+    }
+
+    /**
+     * @dev Withdraw the staked token from liquidity gauge.
+     * @param _amount Amounf of token to withdraw
+     */
+    function withdraw(uint256 _amount) public {
+        require(_amount > 0, "zero amount");
+
+        _checkpoint();
+        _checkpointUser(msg.sender);
+        _checkpointRewards(msg.sender);
+
+        _burn(msg.sender, _amount);
+        _updateLiquidityLimit(msg.sender);
+
+        if (rewardContract != address(0x0)) {
+            IUniPool(rewardContract).withdraw(_amount);
+        }
+
+        IERC20Upgradeable(token).safeTransfer(msg.sender, _amount);
+        emit Withdrawn(msg.sender, _amount);
+    }
+
 }
---
+++ contracts/interfaces/IVotingEscrow.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+/**
+ * @title Interface for voting escrow.
+ */
+interface IVotingEscrow {
+
+    function user_point_epoch(address _account) external view returns (uint256);
+
+    function user_point_history__ts(address _account, uint256 _epoch) external view returns (uint256);
+}
