87fc02370be16298b98c14ddef9c81f89713599f
Shengda <dingshengda@gmail.com>
2021-03-04 00:32:35 -0800

    Deploy liquidity gauge

--- scripts/deploy_single_plus.js
+++ scripts/deploy_single_plus.js
@@ -5,6 +5,9 @@ const CurveSinglePlus = artifacts.require("CurveSinglePlus");
 const CompoundSinglePlus = artifacts.require("CompoundSinglePlus");
 const GaugeController = artifacts.require("GaugeController");
 const GaugeControllerProxy = artifacts.require("GaugeControllerProxy");
+const LiquidityGauge = artifacts.require("LiquidityGauge");
+const LiquidityGaugeProxy = artifacts.require("LiquidityGaugeProxy");
+const VotingEscrow = artifacts.require("VotingEscrow");
 const {renCrv, sbtcCrv, acBTC, cWBTC, renSwap, sbtcSwap} = require('../constant');
 
 module.exports = async function (callback) {
@@ -12,6 +15,7 @@ module.exports = async function (callback) {
         const accounts = await web3.eth.getAccounts();
         const btcPlus = await BTCPlus.at((await PlusProxy.deployed()).address);
         const gaugeController = await GaugeController.at((await GaugeControllerProxy.deployed()).address);
+        const votingEscrow = await VotingEscrow.deployed();
         
         console.log('Deploying renCrv+...');
         const renCrvPlus = await CurveSinglePlus.new();
@@ -19,7 +23,10 @@ module.exports = async function (callback) {
         const proxiedRenCrvPlus = await CurveSinglePlus.at(renCrvPlusProxy.address);
         await proxiedRenCrvPlus.initialize(renSwap, renCrv, '', '');
         await btcPlus.addToken(proxiedRenCrvPlus.address);
+        await gaugeController.addToken(proxiedRenCrvPlus.address);
         console.log(`renCrv+ address: ${proxiedRenCrvPlus.address}`);
+        const renCrvGauge = await deployGauge(proxiedRenCrvPlus.address, gaugeController, votingEscrow);
+        console.log(`renCrv+ gauge: ${renCrvGauge.address}`);
 
         console.log('Deploying sbtcCrv+...');
         const sbtcCrvPlus = await CurveSinglePlus.new();
@@ -27,7 +34,10 @@ module.exports = async function (callback) {
         const proxiedSbtcCrvPlus = await CurveSinglePlus.at(sbtcCrvPlusProxy.address);
         await proxiedSbtcCrvPlus.initialize(sbtcSwap, sbtcCrv, '', '');
         await btcPlus.addToken(proxiedSbtcCrvPlus.address);
+        await gaugeController.addToken(proxiedSbtcCrvPlus.address);
         console.log(`sbtcCrv+ address: ${proxiedSbtcCrvPlus.address}`);
+        const sbtcCrvGauge = await deployGauge(proxiedSbtcCrvPlus.address, gaugeController, votingEscrow);
+        console.log(`sbtcCrv+ gauge: ${sbtcCrvGauge.address}`);
 
         console.log('Deploying acBTC+...');
         const acBtcPlus = await SinglePlus.new();
@@ -35,7 +45,10 @@ module.exports = async function (callback) {
         const proxiedAcBtcPlus = await SinglePlus.at(acBtcPlusProxy.address);
         await proxiedAcBtcPlus.initialize(acBTC, '', '');
         await btcPlus.addToken(proxiedAcBtcPlus.address);
+        await gaugeController.addToken(proxiedAcBtcPlus.address);
         console.log(`acBTC++ address: ${proxiedAcBtcPlus.address}`);
+        const acBtcGauge = await deployGauge(proxiedAcBtcPlus.address, gaugeController, votingEscrow);
+        console.log(`acBtc+ gauge: ${acBtcGauge.address}`);
 
         console.log('Deploying cWBTC+...');
         const cWBTCPlus = await CompoundSinglePlus.new();
@@ -43,10 +56,25 @@ module.exports = async function (callback) {
         const proxiedCWBTCPlus = await CompoundSinglePlus.at(cWBTCPlusProxy.address);
         await proxiedCWBTCPlus.initialize(cWBTC, '', '');
         await btcPlus.addToken(proxiedCWBTCPlus.address);
+        await gaugeController.addToken(proxiedCWBTCPlus.address);
         console.log(`cWBTC++ address: ${proxiedCWBTCPlus.address}`);
+        const cWBTCGauge = await deployGauge(proxiedCWBTCPlus.address, gaugeController, votingEscrow);
+        console.log(`cWBTC+ gauge: ${cWBTCGauge.address}`);
 
         callback();
     } catch (e) {
         callback(e);
     }
+}
+
+
+async function deployGauge(token, gaugeController, votingEscrow) {
+    const accounts = await web3.eth.getAccounts();
+    const gauge = await LiquidityGauge.new();
+    const gaugeProxy = await LiquidityGaugeProxy.new(gauge.address, accounts[1], Buffer.from(''));
+    const proxiedGauge = await LiquidityGauge.at(gaugeProxy.address);
+    await proxiedGauge.initialize(token, gaugeController.address, votingEscrow.address);
+    await gaugeController.addGauge(proxiedGauge.address, web3.utils.toWei("1"));
+
+    return proxiedGauge;
 }
