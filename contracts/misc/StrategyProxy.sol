eb86e5f9e2668e76511c8fdd3e97b0d86534c88b
Shengda <dingshengda@gmail.com>
2021-03-13 15:37:17 -0800

    Deploy strategy to bsc

---
+++ contracts/misc/StrategyProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
+
+/**
+ * @title Proxy for strategies.
+ */
+contract StrategyProxy is TransparentUpgradeableProxy {
+    constructor(address _logic, address _admin, bytes memory _data) TransparentUpgradeableProxy(_logic, _admin, _data) public payable {}
+}
---
+++ scripts/deploy_vbtc_plus.js
@@ -0,0 +1,34 @@
+const CompoundSinglePlus = artifacts.require("CompoundSinglePlus");
+const PlusProxy = artifacts.require("PlusProxy");
+const StrategyVenusBTCVAI = artifacts.require("StrategyVenusBTCVAI");
+const StrategyProxy = artifacts.require("StrategyProxy");
+
+const VBTC = "0x882C173bC7Ff3b7786CA16dfeD3DFFfb9Ee7847B";
+
+module.exports = async function (callback) {
+    try {
+        const accounts = await web3.eth.getAccounts();
+
+        console.log('Deploying vBTC+...');
+        const vBTCPlus = await CompoundSinglePlus.new();
+        const vBTCPlusProxy = await PlusProxy.new(vBTCPlus.address, accounts[1], Buffer.from(''));
+        const proxiedVBTCPlus = await CompoundSinglePlus.at(vBTCPlusProxy.address);
+        await proxiedVBTCPlus.initialize(VBTC, '', '');
+
+        console.log('Deploying vBTC strategy...');
+        const vBTCStrategy = await StrategyVenusBTCVAI.new();
+        const vBTCStrategyProxy = await StrategyProxy.new(vBTCStrategy.address, accounts[1], Buffer.from(''));
+        const proxiedVBTCStrategy = await StrategyVenusBTCVAI.at(vBTCStrategyProxy.address);
+        await proxiedVBTCStrategy.initialize(proxiedVBTCPlus.address);
+
+        await proxiedVBTCPlus.approveStrategy(proxiedVBTCStrategy.address , true);
+
+        console.log(`Proxy admin: ${accounts[1]}`);
+        console.log(`vBTC+: ${proxiedVBTCPlus.address}`);
+        console.log(`vBTC+ strategy: ${proxiedVBTCStrategy.address}`);
+
+        callback();
+    } catch (e) {
+        callback(e);
+    }
+}
