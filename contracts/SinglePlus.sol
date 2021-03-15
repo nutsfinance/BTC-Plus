b4e95ec9f6dc44d1d8dbdb628d13edb462ba646d
Shengda <dingshengda@gmail.com>
2021-03-15 13:25:38 -0700

    Remove strategy interface

--- contracts/SinglePlus.sol
+++ contracts/SinglePlus.sol
@@ -9,7 +9,6 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
-import "./interfaces/IStrategy.sol";
 import "./Plus.sol";
 
 /**
