596cbf2453f4ee2fffbbd6ba08f978182c9d4f00
Shengda <dingshengda@gmail.com>
2021-04-15 00:20:59 -0700

    Upgrade vWBTC+ script

--- scripts/eth/upgrade_vwbtc_plus.js
+++ scripts/eth/upgrade_vwbtc_plus.js
@@ -8,7 +8,8 @@ module.exports = async function (callback) {
         const accounts = await web3.eth.getAccounts();
 
         console.log('Upgrading vWBTC+...');
-        const cWBTCPlusImpl = await VesperWBTCPlus.new();
+        // const cWBTCPlusImpl = await VesperWBTCPlus.new();
+        const cWBTCPlusImpl = await VesperWBTCPlus.at("0x3c12f7d8cbfe49ec341c80cb0e5517638e8f0550");
         const cWBTCPlusProxy = await ERC20Proxy.at(VWBTC_PLUS);
         await cWBTCPlusProxy.upgradeTo(cWBTCPlusImpl.address, {from: accounts[1]});
 
