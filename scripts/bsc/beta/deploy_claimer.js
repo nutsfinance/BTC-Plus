2037c57474a07f84d7b5fbd059967d7ba0cb29ad
Shengda <dingshengda@gmail.com>
2021-04-28 16:30:24 -0700

    Deploy claimer script

--- scripts/bsc/beta/deploy_claimer.js
+++ scripts/bsc/beta/deploy_claimer.js
@@ -1,13 +1,8 @@
 const Claimer = artifacts.require("Claimer");
 const GaugeController = artifacts.require("GaugeController");
 
-const BN = web3.utils.BN;
-const toWei = web3.utils.toWei;
-const MAX = new BN(2).pow(new BN(256)).sub(new BN(1));
-const BTCB = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
-
 const GAUGE_CONTROLLER = '0x19F8246F5eadfE0dc46E62BE5fc995Aca16efAb4';
-const VOTING_ESCROW = '0xd8103E3203E40D1b6F01E9d5db55256f610FB68F';
+const VOTING_ESCROW = '0x17580529d7a21be535E05960b167c4d6c961947c';
 const AC = '0xe9c1B8993A8750Ae65607Ef91ebCdE595DeB4EC3';
 
 module.exports = async function (callback) {
--- scripts/bsc/prod/deploy_claimer.js
+++ scripts/bsc/prod/deploy_claimer.js
@@ -1,5 +1,7 @@
 const Claimer = artifacts.require("Claimer");
+const GaugeController = artifacts.require("GaugeController");
 
+const GAUGE_CONTROLLER = '0xc7cAF20bD0C16CCBA7673b0848C4B503325256A4';
 const VOTING_ESCROW = '0xd8103E3203E40D1b6F01E9d5db55256f610FB68F';
 const AC = '0x5b45a9be49c94236e127efcc601b7e7a1a485d0a';
 
@@ -8,6 +10,9 @@ module.exports = async function (callback) {
         console.log('Deploying Claimer...');
         const claimer = await Claimer.new(VOTING_ESCROW, AC);
         console.log(`Claimer: ${claimer.address}`);
+
+        const gaugeController = await GaugeController.at(GAUGE_CONTROLLER);
+        await gaugeController.setClaimer(claimer.address, true);
 
         callback();
     } catch (e) {
